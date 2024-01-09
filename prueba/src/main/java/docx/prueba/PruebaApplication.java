9243cf6d5b3344105f0b325120aa8e49d08a1508
Vontrailop <davidperez@utez.edu.mx>
2024-01-09 17:49:42 -0600

    progress

--- prueba/src/main/java/docx/prueba/PruebaApplication.java
+++ prueba/src/main/java/docx/prueba/PruebaApplication.java
@@ -5,40 +5,34 @@ import java.util.List;
 
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
-import javax.xml.soap.Text;
 
+import org.docx4j.model.datastorage.migration.VariablePrepare;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
-import org.docx4j.wml.P;
+import org.docx4j.wml.Text;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PruebaApplication {
 
-	public static void main(String[] args) throws Docx4JException, JAXBException {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(PruebaApplication.class, args);
 
-		// WordprocessingMLPackage wordPackage =
-		// WordprocessingMLPackage.createPackage();
-		// MainDocumentPart mainDocumentPart = wordPackage.getMainDocumentPart();
-		// mainDocumentPart.addStyledParagraphOfText("Title", "Hello World!");
-		// mainDocumentPart.addParagraphOfText("Welcome To Baeldung");
-		// File exportFile = new File("welcome.docx");
-		// wordPackage.save(exportFile);
+		// WordprocessingMLPackage wordmlp = WordprocessingMLPackage.createPackage();
 
-		File doc = new File("Prueba1.docx");
+		// wordmlp.save(new File("Helloworld.docx"));
+
+		File doc = new File("C:/Desarrollo/Docx/prueba/FileFormat.docx");
+		System.out.println(doc);
 		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(doc);
 		MainDocumentPart mainDocumentPart = wordMLPackage.getMainDocumentPart();
-		String textNodesXPath = "//w:t";
-		List<Object> textNodes = mainDocumentPart
-				.getJAXBNodesViaXPath(textNodesXPath, true);
-		for (Object obj : textNodes) {
-			Text text = (Text) ((JAXBElement) obj).getValue();
-			String textValue = text.getValue();
-			System.out.println(textValue);
-		}
+		
+		java.util.HashMap mappings = new java.util.HashMap();
+		VariablePrepare.prepare(wordMLPackage);//see notes
+		mappings.put("myField", "foo");
+		wordMLPackage.getMainDocumentPart().variableReplace(mappings);
 
 	}
 
