682032de36447ba288329d47674df485c505dc5b
CokerAlcocer <20203tn066@utez.edu.mx>
2024-01-17 13:00:36 -0600

    [UPDATE] Ya se accede al encabezado del documento

--- prueba/src/main/java/docx/prueba/PruebaApplication.java
+++ prueba/src/main/java/docx/prueba/PruebaApplication.java
@@ -16,10 +16,7 @@ import org.docx4j.openpackaging.parts.WordprocessingML.HeaderPart;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
 import org.docx4j.openpackaging.parts.relationships.RelationshipsPart;
 import org.docx4j.relationships.Relationship;
-import org.docx4j.wml.HeaderReference;
-import org.docx4j.wml.P;
-import org.docx4j.wml.SectPr;
-import org.docx4j.wml.Text;
+import org.docx4j.wml.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -29,8 +26,9 @@ public class PruebaApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(PruebaApplication.class, args);
 
-		File doc = new File("C:/Desarrollo/Docx/prueba/1. OFICIO DE COMISIÓN.docx");
-		//System.out.println(doc);
+		File doc = new File("/Users/alcoker/Desktop/Docx/prueba/1. OFICIO DE COMISIÓN.docx");
+		//File doc = new File("C:/Desarrollo/Docx/prueba/1. OFICIO DE COMISIÓN.docx");
+
 		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(doc);
 
 		java.util.HashMap mappings = new java.util.HashMap();
@@ -38,39 +36,84 @@ public class PruebaApplication {
 		mappings.put("folioInspeccion", "DW56165");
 		mappings.put("dia", "15");
 		mappings.put("anoLetra", "Dos mil veinticuatro");
-		// mappings.put("myField", "486qw4d68qw");
+
 
 		wordMLPackage.getMainDocumentPart().variableReplace(mappings);
 
-		
+		SectionWrapper section = wordMLPackage.getDocumentModel().getSections().get(0);
+		if (section.getHeaderFooterPolicy() != null) {
+			HeaderPart header = section.getHeaderFooterPolicy().getHeader(0);
 
-		wordMLPackage.save(new File("C:/Desarrollo/Docx/prueba/Resultado.docx"));
+			for (Object obj : header.getContent()) {
+				if (obj instanceof JAXBElement) {
+					if(((JAXBElement<?>) obj).getValue() instanceof Tbl) {
+						Tbl table = (Tbl) ((JAXBElement<?>) obj).getValue();
+
+						for(Object row : table.getContent()) {
+							if(row instanceof Tr) {
+
+								for(Object cell : ((Tr) row).getContent()) {
+									Tc tCell = (Tc) ((JAXBElement<?>) cell).getValue();
+
+									setTextInCell(tCell, "FolioNuevoDeLaInspección");
+								}
+							}
+						}
+					}
+				}
+			}
+
+			String xpath = "w:r[w:t[contains(text(),'myField')]]";
+			List<Object> list = section.getHeaderFooterPolicy().getDefaultHeader().getJAXBNodesViaXPath(xpath, true);
+
+
+			section.getHeaderFooterPolicy().getDefaultHeader().variableReplace(mappings);
+			section.getHeaderFooterPolicy().getDefaultFooter().variableReplace(mappings);
+
+		}
+
+		wordMLPackage.save(new File("/Users/alcoker/Desktop/Docx/prueba/Resultado.docx"));
+		//wordMLPackage.save(new File("C:/Desarrollo/Docx/prueba/Resultado.docx"));
 	}
 
+//	private static String getTextInCell(Tc cell) {
+//		StringBuilder text = new StringBuilder();
+//		List<Object> cellContent = cell.getContent();
+//		for (Object obj : cellContent) {
+//			if (obj instanceof P) {
+//				P paragraph = (P) obj;
+//				text.append(getTextInParagraph(paragraph));
+//			}
+//		}
+//		return text.toString();
+//	}
+//
+//	private static String getTextInParagraph(P paragraph) {
+//		StringBuilder text = new StringBuilder();
+//		List<Object> texts = paragraph.getContent();
+//		for (Object textObj : texts) {
+//			if (textObj instanceof Text) {
+//				Text t = (Text) textObj;
+//				text.append(t.getValue());
+//			}
+//		}
+//		return text.toString();
+//	}
 
-	
-		// for (SectionWrapper section : wordMLPackage.getDocumentModel().getSections()) {
-		// 	if (section.getHeaderFooterPolicy() != null) {
-		// 		//System.out.println(section.getHeaderFooterPolicy().getDefaultHeader());
-		// 		//System.out.println("Contenido de la cabecera: " + section.getHeaderFooterPolicy().getDefaultHeader().getContent());
+	private static void setTextInCell(Tc cell, String newText) {
+		// Crear un nuevo run
+		R newRun = new R();
 
-		// 		// for (Object obj : section.getHeaderFooterPolicy().getDefaultHeader().getContent()) {
-		// 		// 	if (obj instanceof P) {
-		// 		// 		P paragraph = (P) obj;
-		// 		// 		// Reemplazar directamente el texto en el párrafo
+		// Crear un nuevo párrafo con el nuevo run
+		P newParagraph = new P();
+		newParagraph.getContent().add(newRun);
 
-		// 		// 		Text text = (Text) paragraph.getContent().get(0);
-		// 		// 		text.setValue("NuevoValor");
-		// 		// 	}
-		// 		// }
+		// Agregar el texto al nuevo run
+		Text newTextObj = new Text();
+		newTextObj.setValue(newText);
+		newRun.getContent().add(newTextObj);
 
-		// 		String xpath = "//w:r[w:t[contains(text(),'myField')]]";
-		// 		List<Object> list = section.getHeaderFooterPolicy().getDefaultHeader().getJAXBNodesViaXPath(xpath, true);
-
-
-		// 		//section.getHeaderFooterPolicy().getDefaultHeader().variableReplace(mappings);
-		// 		//section.getHeaderFooterPolicy().getDefaultFooter().variableReplace(mappings);
-		
-		// 	}
-		// }
+		// Agregar el nuevo párrafo al contenido de la celda
+		cell.getContent().add(newParagraph);
+	}
 }
